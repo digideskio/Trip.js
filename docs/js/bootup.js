4d7a03236511875e9e9269938cf6756234716a6f
EragonJ <eragonj@eragonj.me>
2016-08-05 15:05:23 +0800

    add some metadata

--- docs/js/bootup.js
+++ docs/js/bootup.js
@@ -1,6 +1,7 @@
-requirejs.config({
-  paths: {
-    jquery: base_url + '/vendor/jquery/jquery'
-  }
-});
-
+(function() {
+  requirejs.config({
+    paths: {
+      jquery: '/vendor/jquery/jquery'
+    }
+  });
+})();
